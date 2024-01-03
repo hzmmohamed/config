befb123c83c83f282dcdacadad8ced168e62acb3
Hazem Fahmi <hzmmohamed@gmail.com>
2024-01-04 00:35:18 +0200

    Add comment for obsidian fix

--- modules/nixos/tools/obsidian/default.nix
+++ modules/nixos/tools/obsidian/default.nix
@@ -20,6 +20,7 @@ in {
       ];
 
       # TODO: This overwrites the env var rather than append to it. The original value has something to do with pipewire. A better approach is to wrap obsidian with wrapProgram 
+      # https://discourse.nixos.org/t/electron-apps-cant-find-opengl/35713/14?u=hfahmi
       home.sessionVariables = {
         LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
       };
