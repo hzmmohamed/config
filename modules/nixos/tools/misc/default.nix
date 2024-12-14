d4c334b209f5482f25b95779e04386fda081d5cf
Hazem Fahmi <hzmmohamed@gmail.com>
2024-12-15 00:35:30 +0200

    Configured Atuin sync and UX

--- modules/nixos/tools/misc/default.nix
+++ modules/nixos/tools/misc/default.nix
@@ -26,6 +26,19 @@ in {
         atuin = {
           enable = true;
           enableFishIntegration = true;
+          settings = {
+            auto_sync = true;
+            sync_frequency = "5m";
+            sync_address = "https://api.atuin.sh";
+            inline_height = 15;
+            enter_accept = false;
+            keymap_mode = "vim-insert";
+            keymap_cursor = {
+              emacs = "blink-block";
+              vim_insert = "blink-bar";
+              vim_normal = "steady-block";
+            };
+          };
         };
 
         lsd = {
