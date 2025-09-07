83dfc11a4107dc2f4e6843c2c77104cbc6ad5f93
Hazem Fahmi <hzmmohamed@gmail.com>
2025-09-08 02:55:49 +0300

    Update to latest

--- modules/nixos/apps/vscode/default.nix
+++ modules/nixos/apps/vscode/default.nix
@@ -18,6 +18,7 @@ in {
         package = pkgs.vscodium;
         mutableExtensionsDir = true; # default
         profiles.default.extensions = with pkgs.vscode-extensions; [
+          pkgs.unstable.vscode-extensions.continue.continue
 
           tomoki1207.pdf
           # Theming
